0a02d5bc9a887f2b1fd2dd12ca36c82f2831503d
BlaneTorregrossa <blanet97@cox.net>
2016-12-08 10:45:42 -0600

    changes to matracies

--- MathLibraries/Header.h
+++ MathLibraries/Header.h
@@ -2,14 +2,6 @@
 #include <iostream>
 
 /*
-Currently working on:
-Fixing problems with classes in the Math Libraries Vectors
-
-Stopping point was:
-Creating the functions for the Vector classes
-*/
-
-/*
 v = Vectors		m = matrix		f = float
 v = v + v		v = v - v		v = v * f		   v = m * v		m = m * m
 f = v.dot(v)	v = v.cross(v)	f = v.magnitude()  v.noramlise()
@@ -31,7 +23,7 @@ Matrix2
 Matrix3
 Matrix4
 */
-
+ 
 class Vector2
 {
 
@@ -71,10 +63,12 @@ public:
 		y = (y / Magnitude());
 	}
 
+	float DotProd(Vector2)
+	{
+		float dot = (x * x) + (y * y);
+		return dot;
+	}
 
-
-	
-	
 };
 
 class Vector3
@@ -113,6 +107,13 @@ public:
 		y = (y / Magnitude());
 		z = (z / Magnitude());
 	}
+
+	float DotProd(Vector3)
+	{
+		float dot = (x * x) + (y * y) + (z * z);
+		return dot;
+	}
+
 
 };
 
@@ -154,6 +155,14 @@ public:
 		z = (z / Magnitude());
 		w = (w / Magnitude());
 	}
+
+
+	float DotProd(Vector4)
+	{
+		float dot = (x * x) + (y * y) + (z * z) + (w * W);
+		return dot;
+	}
+
 };
 
 
@@ -165,8 +174,34 @@ private:
 
 public:
 	Matrix2() {};
+	Matrix2(float xx1, float xx2, float xx3, float xx4) {}
 
+	Matrix2 operator +(const Matrix2 &T)
+	{
+		return Matrix2(x1 + T.x1, x2 + T.x2, x3 + T.x3, x4 + T.x4);
+	}
 
+	Matrix2 operator -(const Matrix2 &T)
+	{
+		return Matrix2(x1 - T.x1, x2 - T.x2, x3 - T.x3, x4 - T.x4);
+	}
+
+	Matrix2 operator *(const Matrix2 &T)
+	{
+		return Matrix2(x1 * T.x1, x2 * T.x2, x3 * T.x3, x4 * T.x4);
+	}
+	
+	//This most likely could be done in another way that is easier to understand.
+	float CrossProd(Matrix2)
+	{
+		float x1Tmp, x2Tmp, x3Tmp, x4Tmp;
+		x1Tmp = (x1 * x1) + (x2 * x3);
+		x2Tmp = (x1 * x2) + (x2 * x4);
+		x3Tmp = (x3 * x1) + (x4 * x3);
+		x4Tmp = (x3 * x2) + (x4 * x4);
+		return x1Tmp, x2Tmp, x3Tmp, x4Tmp;
+
+	}
 };
 
 
@@ -179,7 +214,37 @@ private:
 
 public:
 	Matrix3() {};
+	Matrix3(float xx1, float xx2, float xx3, float xx4, float xx5, float xx6, float xx7, float xx8, float xx9) {}
 
+	Matrix3 operator +(const Matrix3 &T)
+	{
+		return Matrix3(x1 + T.x1, x2 + T.x2, x3 + T.x3, x4 + T.x4, x5 + T.x5, x6 + T.x6, x7 + T.x7, x8 + T.x8, x9 + T.x9);
+	}
+
+	Matrix3 operator -(const Matrix3 &T)
+	{
+		return Matrix3(x1 - T.x1, x2 - T.x2, x3 - T.x3, x4 - T.x4, x5 - T.x5, x6 - T.x6, x7 - T.x7, x8 - T.x8, x9 - T.x9);
+	}
+
+	Matrix3 operator *(const Matrix3 &T)
+	{
+		return Matrix3(x1 * T.x1, x2 * T.x2, x3 * T.x3, x4 * T.x4, x5 * T.x5, x6 * T.x6, x7 * T.x7, x8 * T.x8, x9 * T.x9);
+	}
+
+	float CrossProd(Vector2)
+	{
+		float x1Tmp, x2Tmp, x3Tmp, x4Tmp, x5Tmp, x6Tmp, x7Tmp, x8Tmp, x9Tmp;
+		x1Tmp = (x1 * x1) + (x2 * x4) + (x3 * x7);
+		x2Tmp = (x1 * x2) + (x2 * x5) + (x3 * x9);
+		x3Tmp
+		x4Tmp
+		x5Tmp
+		x6Tmp
+		x7Tmp
+		x8Tmp
+		x9Tmp
+		return x1Tmp, x2Tmp, x3Tmp, x4Tmp, x5Tmp, x6Tmp, x7Tmp, x8Tmp, x9Tmp;
+	}
 
 };
 
@@ -193,6 +258,20 @@ private:
 
 public:
 	Matrix4() {};
+	Matrix4(float xx1, float xx2, float xx3, float xx4, float xx5, float xx6, float xx7, float xx8, float xx9, float xx10, float xx11, float xx12, float xx13, float xx14, float xx15, float xx16) {}
 
+	Matrix4 operator +(const Matrix4 &T)
+	{
+		return Matrix4(x1 + T.x1, x2 + T.x2, x3 + T.x3, x4 + T.x4, x5 + T.x5, x6 + T.x6, x7 + T.x7, x8 + T.x8, x9 + T.x9, x10 + T.x10, x11 + T.x11, x12 + T.x12, x13 + T.x13, x14 + T.x14, x15 + T.x15, x16 + T.x16);
+	}
 
+	Matrix4 operator -(const Matrix4 &T)
+	{
+		return Matrix4(x1 - T.x1, x2 - T.x2, x3 - T.x3, x4 - T.x4, x5 - T.x5, x6 - T.x6, x7 - T.x7, x8 - T.x8, x9 - T.x9, x10 - T.x10, x11 - T.x11, x12 - T.x12, x13 - T.x13, x14 - T.x14, x15 - T.x15, x16 - T.x16);
+	}
+
+	Matrix4 operator *(const Matrix4 &T)
+	{
+		return Matrix4(x1 * T.x1, x2 * T.x2, x3 * T.x3, x4 * T.x4, x5 * T.x5, x6 * T.x6, x7 * T.x7, x8 * T.x8, x9 * T.x9, x10 * T.x10, x11 * T.x11, x12 * T.x12, x13 * T.x13, x14 * T.x14, x15 * T.x15, x16 * T.x16);
+	}
 };

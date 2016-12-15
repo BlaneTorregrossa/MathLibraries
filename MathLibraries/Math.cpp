2692a58c13f20f927899782e61adbbd7cbee9f36
BlaneTorregrossa <blanet97@cox.net>
2016-12-15 17:42:59 -0600

    .

--- MathLibraries/Math.cpp
+++ MathLibraries/Math.cpp
@@ -11,19 +11,10 @@ Vector2 Vector2::operator-(const Vector2 & T)
 	return Vector2(x - T.x, y - T.y);
 }
 
-Vector2 Vector2::operator += (const Vector2 & T)
+Vector2 Vector2::operator*(const float &T)
 {
-	return Vector2(x += T.x, y += T.y);
-}
 
-Vector2 Vector2::operator -= (const Vector2 & T)
-{
-	return Vector2(x -= T.x, y -= T.y);
-}
-
-Vector2 Vector2::operator*(const Vector2 &T)
-{
-	return Vector2(x * T.x, y * T.y);
+	return Vector2(x * T, y * T);
 }
 
 float Vector2::Magnitude()
@@ -47,6 +38,7 @@ float Vector2::DotProd(Vector2)
 
 //-------------------------------------------------------------------------
 
+//-------------------------------------------------------------------------
 
 Vector3 Vector3::operator+(const Vector3 & T)
 {
@@ -58,19 +50,9 @@ Vector3 Vector3::operator-(const Vector3 & T)
 	return Vector3(x - T.x, y - T.y, z + T.z);
 }
 
-Vector3 Vector3::operator+=(const Vector3 & T)
+Vector3 Vector3::operator*(const float & T)
 {
-	return Vector3(x += T.x, y += T.y, z += T.z);
-}
-
-Vector3 Vector3::operator-=(const Vector3 & T)
-{
-	return Vector3(x -= T.x, y -= T.y, z -= T.z);
-}
-
-Vector3 Vector3::operator*(const Vector3 & T)
-{
-	return Vector3(x * T.x, y * T.y, z * T.z);
+	return Vector3(x * T, y * T, z * T);
 }
 
 float Vector3::Magnitude()
@@ -95,6 +77,8 @@ float Vector3::DotProd(Vector3)
 
 //-------------------------------------------------------------------------
 
+//-------------------------------------------------------------------------
+
 
 Vector4 Vector4::operator+(const Vector4 & T)
 {
@@ -106,19 +90,9 @@ Vector4 Vector4::operator-(const Vector4 & T)
 	return Vector4(x - T.x, y - T.y, z - T.z, w - T.w);
 }
 
-Vector4 Vector4::operator+=(const Vector4 & T)
+Vector4 Vector4::operator*(const float & T)
 {
-	return Vector4(x += T.x, y += T.y, z += T.z, w += T.w);
-}
-
-Vector4 Vector4::operator-=(const Vector4 & T)
-{
-	return Vector4(x -= T.x, y -= T.y, z -= T.z, w -= T.w);
-}
-
-Vector4 Vector4::operator*(const Vector4 & T)
-{
-	return Vector4(x * T.x, y * T.y, z * T.z, w * T.w);
+	return Vector4(x * T, y * T, z * T, w * T);
 }
 
 float Vector4::Magnitude()
@@ -144,22 +118,44 @@ float Vector4::DotProd(Vector4)
 
 //-------------------------------------------------------------------------
 
+//-------------------------------------------------------------------------
 
-Matrix2 Matrix2::operator +(const Matrix2 &T)
+
+
+
+Matrix2 Matrix2::operator* (const Matrix2 &T)
 {
-	return Matrix2(x1 + T.x1, x2 + T.x2, x3 + T.x3, x4 + T.x4);
+	return Matrix2((x1 * T.x1) + (x2 * T.x3), (x1 * T.x2) + (x2 * T.x4),	// row 1
+		(x3 * T.x1) + (x4 * T.x3), (x3 * T.x2) + (x4 * T.x4));				// row 2
 }
 
-//-------------------------------------------------------------------------
-
-
-//Matrix3
-
 
 //-------------------------------------------------------------------------
 
+//-------------------------------------------------------------------------
 
-//Matrix4
+
+Matrix3 Matrix3::operator* (const Matrix3 &T)
+{
+	return Matrix3((x1 * T.x1) + (x2 * T.x4) + (x3 * T.x7), (x1 * T.x2) + (x2 * T.x5) + (x3 * T.x8), (x1 * T.x3) + (x2 * T.x6) + (x3 * T.x9),	// row 1
+		(x4 * T.x1) + (x5 * T.x4) + (x6 * T.x7), (x4 * T.x2) + (x5 * T.x5) + (x6 * T.x8), (x4 * T.x3) + (x5 * T.x6) + (x6 * T.x9),				// row 2
+		(x7 * T.x1) + (x8 * T.x4) + (x9 * T.x7), (x7 * T.x2) + (x8 * T.x5) + (x9 * T.x8), (x7 * T.x3) + (x8 * T.x6) + (x9 * T.x9));				// row 3
+}
+
+
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+
+
+Matrix4 Matrix4::operator* (const Matrix4 &T)
+{
+	return Matrix4((x1 * T.x1) + (x2 * T.x5) + (x3 * T.x9) + (x4 * T.x13), (x1 * T.x2) + (x2 * T.x6) + (x3 * T.x10) + (x4 * T.x14), (x1 * T.x3) + (x2 * T.x7) + (x3 * T.x11) + (x4 * T.x15), (x1 * T.x4) + (x2 * T.x8) + (x3 * T.x12) + (x4 * T.x16),		// row 1
+		(x5 * T.x1) + (x6 * T.x5) + (x7 * T.x9) + (x8 * T.x13), (x5 * T.x2) + (x6 * T.x6) + (x7 * T.x10) + (x8 * T.x14), (x5 * T.x3) + (x6 * T.x7) + (x7 * T.x11) + (x8 * T.x15), (x5 * T.x4) + (x6 * T.x8) + (x7 * T.x12) + (x8 * T.x16),					// row 2
+		(x9 * T.x1) + (x10 * T.x5) + (x11 * T.x9) + (x12 * T.x13), (x9 * T.x2) + (x10 * T.x6) + (x11 * T.x10) + (x12 * T.x14), (x9 * T.x3) + (x10 * T.x7) + (x11 * T.x11) + (x12 * T.x15), (x9 * T.x4) + (x10 * T.x8) + (x11 * T.x12) + (x12 * T.x16),		// row 3
+		(x13 * T.x1) + (x14 * T.x5) + (x15 * T.x9) + (x16 * T.x13), (x13 * T.x2) + (x14 * T.x6) + (x15 * T.x10) + (x16 * T.x14), (x13 * T.x3) + (x14 * T.x7) + (x15 * T.x11) + (x16 * T.x15), (x13 * T.x4) + (x14 * T.x8) + (x15 * T.x12) + (x16 * T.x16));	// row 4
+}
+
 
 
 //-------------------------------------------------------------------------
--- MathLibraries/Math.h
+++ MathLibraries/Math.h
@@ -18,7 +18,7 @@ Matrix2
 Matrix3
 Matrix4
 */
- 
+
 
 class Vector2
 {
@@ -31,18 +31,14 @@ public:
 	Vector2(float xx, float yy) {}
 
 	Vector2 operator +(const Vector2 &T);
-	
+
 	Vector2 operator -(const Vector2 &T);
 
-	Vector2 operator += (const Vector2 &T);
+	Vector2 operator *(const float &T);
 
-	Vector2 operator -= (const Vector2 &T);
-
-	Vector2 operator *(const Vector2 &T);
-	
 	// Magnitude is needed for other functions in this class to work properly.
 	float Magnitude(); // Function gives the squareroot of the sum of varible of type int x squared and type int y squared.
-	
+
 	// Needs to use mag to Normalise the Vector
 	Vector2 Normalise();
 
@@ -64,19 +60,16 @@ public:
 
 	Vector3 operator -(const Vector3 &T);
 
-	Vector3 operator += (const Vector3 &T);
+	Vector3 operator *(const float &T);
 
-	Vector3 operator -= (const Vector3 &T);
 
-	Vector3 operator *(const Vector3 &T);
-	
 	// Magnitude is needed for other functions in this class to work properly.
 	float Magnitude(); // Function gives the squareroot of the sum of varible of type int x squared type int y squared and type int z squared.
-	
+
 	Vector3 Normalise();
-	
+
 	float DotProd(Vector3);
-	
+
 };
 
 class Vector4
@@ -93,19 +86,16 @@ public:
 
 	Vector4 operator -(const Vector4 &T);
 
-	Vector4 operator +=(const Vector4 &T);
+	Vector4 operator *(const float &T);
 
-	Vector4 operator -=(const Vector4 &T);
-
-	Vector4 operator *(const Vector4 &T);
 
 	// Magnitude is needed for other functions in this class to work properly.
 	float Magnitude(); // Function gives the squareroot of the sum of varible of type int x squared type int y squared type int z squared and int w squared.
 
 	Vector4 Normalise();
-	 
+
 	float DotProd(Vector4);
-	
+
 };
 
 
@@ -119,29 +109,12 @@ public:
 	Matrix2() {};
 	Matrix2(float xx1, float xx2, float xx3, float xx4) {}
 
-	Matrix2 operator +(const Matrix2 &T);
 
-	Matrix2 operator -(const Matrix2 &T)
-	{
-		return Matrix2(x1 - T.x1, x2 - T.x2, x3 - T.x3, x4 - T.x4);
-	}
-
-	Matrix2 operator *(const Matrix2 &T)
-	{
-		return Matrix2(x1 * T.x1, x2 * T.x2, x3 * T.x3, x4 * T.x4);
-	}
+	Matrix2 operator *(const Matrix2 &T);
 	
-	//This most likely could be done in another way that is easier to understand.
-	float CrossProd(Matrix2)
-	{
-		float x1Tmp, x2Tmp, x3Tmp, x4Tmp;
-		x1Tmp = (x1 * x1) + (x2 * x3);
-		x2Tmp = (x1 * x2) + (x2 * x4);
-		x3Tmp = (x3 * x1) + (x4 * x3);
-		x4Tmp = (x3 * x2) + (x4 * x4);
-		return x1Tmp, x2Tmp, x3Tmp, x4Tmp;
 
-	}
+	//float CrossProd(Matrix2);
+
 };
 
 
@@ -156,35 +129,11 @@ public:
 	Matrix3() {};
 	Matrix3(float xx1, float xx2, float xx3, float xx4, float xx5, float xx6, float xx7, float xx8, float xx9) {}
 
-	Matrix3 operator +(const Matrix3 &T)
-	{
-		return Matrix3(x1 + T.x1, x2 + T.x2, x3 + T.x3, x4 + T.x4, x5 + T.x5, x6 + T.x6, x7 + T.x7, x8 + T.x8, x9 + T.x9);
-	}
+	
+	Matrix3 operator *(const Matrix3 &T);
 
-	Matrix3 operator -(const Matrix3 &T)
-	{
-		return Matrix3(x1 - T.x1, x2 - T.x2, x3 - T.x3, x4 - T.x4, x5 - T.x5, x6 - T.x6, x7 - T.x7, x8 - T.x8, x9 - T.x9);
-	}
 
-	Matrix3 operator *(const Matrix3 &T)
-	{
-		return Matrix3(x1 * T.x1, x2 * T.x2, x3 * T.x3, x4 * T.x4, x5 * T.x5, x6 * T.x6, x7 * T.x7, x8 * T.x8, x9 * T.x9);
-	}
-
-	float CrossProd(Vector2)
-	{
-		float x1Tmp, x2Tmp, x3Tmp, x4Tmp, x5Tmp, x6Tmp, x7Tmp, x8Tmp, x9Tmp;
-		x1Tmp = (x1 * x1) + (x2 * x4) + (x3 * x7);
-		x2Tmp = (x1 * x2) + (x2 * x5) + (x3 * x9);
-		x3Tmp
-		x4Tmp
-		x5Tmp
-		x6Tmp
-		x7Tmp
-		x8Tmp
-		x9Tmp
-		return x1Tmp, x2Tmp, x3Tmp, x4Tmp, x5Tmp, x6Tmp, x7Tmp, x8Tmp, x9Tmp;
-	}
+	//float CrossProd(Matrix3);
 
 };
 
@@ -200,18 +149,10 @@ public:
 	Matrix4() {};
 	Matrix4(float xx1, float xx2, float xx3, float xx4, float xx5, float xx6, float xx7, float xx8, float xx9, float xx10, float xx11, float xx12, float xx13, float xx14, float xx15, float xx16) {}
 
-	Matrix4 operator +(const Matrix4 &T)
-	{
-		return Matrix4(x1 + T.x1, x2 + T.x2, x3 + T.x3, x4 + T.x4, x5 + T.x5, x6 + T.x6, x7 + T.x7, x8 + T.x8, x9 + T.x9, x10 + T.x10, x11 + T.x11, x12 + T.x12, x13 + T.x13, x14 + T.x14, x15 + T.x15, x16 + T.x16);
-	}
+	
+	Matrix4 operator *(const Matrix4 &T);
+	
 
-	Matrix4 operator -(const Matrix4 &T)
-	{
-		return Matrix4(x1 - T.x1, x2 - T.x2, x3 - T.x3, x4 - T.x4, x5 - T.x5, x6 - T.x6, x7 - T.x7, x8 - T.x8, x9 - T.x9, x10 - T.x10, x11 - T.x11, x12 - T.x12, x13 - T.x13, x14 - T.x14, x15 - T.x15, x16 - T.x16);
-	}
+	//float CrossProd(Matrix4);
 
-	Matrix4 operator *(const Matrix4 &T)
-	{
-		return Matrix4(x1 * T.x1, x2 * T.x2, x3 * T.x3, x4 * T.x4, x5 * T.x5, x6 * T.x6, x7 * T.x7, x8 * T.x8, x9 * T.x9, x10 * T.x10, x11 * T.x11, x12 * T.x12, x13 * T.x13, x14 * T.x14, x15 * T.x15, x16 * T.x16);
-	}
 };
